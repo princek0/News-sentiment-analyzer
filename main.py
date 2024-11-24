b988058e6102919a9c642981d53f17fc9b87f137
Prince <yuviyukt@googlemail.com>
2024-11-24 20:27:39 +0000

    Refining lexicon with custom terms

--- main.py
+++ main.py
@@ -13,8 +13,44 @@ h1_text = h1.get_text(strip=True)
 
 # NLTK sentiment analyzer
 
-nltk.download('vader_lexicon') # a pretrained sentiment analyzer
 sia = SentimentIntensityAnalyzer() # Initialise sentiment analyzer
+
+# sentiment lexicon
+nltk.download('vader_lexicon') # a pretrained sentiment lexicon
+financial_lexicon = { # custom lexicon for financial extension
+    "bullish": 3.0,
+    "rally": 2.5,
+    "outperform": 1.5,
+    "strong": 1.0,
+    "expansion": 2.0,
+    "exceeded": 2.0,
+    "launching": 1.5,
+    "bearish": -3.0,
+    "loss": -2.5,
+    "profit warning": -3.5,
+    "miss estimates": -2.5,
+    "underperform": -2.0,
+    "weak": -3.0,
+    "selloff": -2.5,
+    "layoff": -1.5,
+    "default": -3.0,
+    "acquisition": 2.5,
+    "merger": 1.5,
+    "dividend": 1.0,
+    "downgrade": -2.5,
+    "upgrade": 2.5,
+    "maintain": 0.0,
+    "recession": -2.5,
+    "inflation": -1.5,
+    "steady": 0.5,
+    "stable": 0.5,
+}
+
+sia.lexicon.update(financial_lexicon) # updating pretrained lexicon
+
+
+
+
 
 sentiment = sia.polarity_scores(h1_text) # analyzing sentiment
 compound_score = sentiment['compound']
