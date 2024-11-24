2a4617038a8b484bb2542699c6bb46f88500d217
Prince <yuviyukt@googlemail.com>
2024-11-24 13:30:43 +0000

    Used NLTK for sentiment analysis
    Followed a tutorial to use a pretrained sentiment analyzer to perform sentiment analysis on yahoo finance headlines I scraped

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import requests
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url='https://uk.finance.yahoo.com/news/mixing-it-up-porsche-unveils-a-new-gas-powered-911-carrera-t-and-electric-taycan-gts-160019197.html'
+# Yahoo finance headlien scraper
+
+url= input("Enter Yahoo finacne URL:")
 page = requests.get(url)
 soup = BeautifulSoup(page.text, features="html.parser")
 h1 = soup.find('h1', class_='cover-title')
 h1_text = h1.get_text(strip=True) 
 
+# NLTK sentiment analyzer
+
+nltk.download('vader_lexicon') # a pretrained sentiment analyzer
+sia = SentimentIntensityAnalyzer() # Initialise sentiment analyzer
+
+sentiment = sia.polarity_scores(h1_text) # analyzing sentiment
+compound_score = sentiment['compound']
+
+# setting up human readable categories based on the compound_score
+if compound_score > 0.05:
+    sentiment_category = 'positive'
+elif compound_score < -0.05:
+    sentiment_category = 'negative'
+else:
+    sentiment_category = 'neutral'
+
 print(f"{h1_text}")
+print(sentiment)
+print(f"Headline sentiment: {sentiment_category}")
